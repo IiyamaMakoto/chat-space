6ed02ce28292221fb7686eb882bf3c59559b4e38
IiyamaMakoto <m.iiyama@gmail.com>
2019-10-22 19:28:41 +0900

    some commits
    日時表記の修正、
    リロードしなくても正常にJSが動作、
    その他修正

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,29 +1,39 @@
 $(function() {
 
+  function scrollLast(){
+    $('.messages__message').animate({
+      scrollTop:$('.last_message').offset().top});  
+  }
+  scrollLast();
+
   function buildHTML(message){
     if (message.image.url == null) {
-      var html = `<div class="messages__message--user">
-                    ${message.user.name}
-                    <div class="messages__message--time">
-                      ${message.created_at}
+      var html = `<div class="messages__message--box last_message">
+                    <div class="messages__message--user">
+                      ${message.user.name}
+                      <div class="messages__message--time">
+                        ${message.created_at}
+                      </div>
                     </div>
-                  </div>
-                  <div class="messages__message--text">
-                    ${message.message}
+                    <div class="messages__message--text">
+                      ${message.message}
+                    </div>
                   </div>`
     }
     else {
-      var html = `<div class="messages__message--user">
-                    ${message.user.name}
-                    <div class="messages__message--time">
-                      ${message.created_at}
+      var html = `<div class="messages__message--box last_message">
+                    <div class="messages__message--user">
+                      ${message.user.name}
+                      <div class="messages__message--time">
+                        ${message.created_at}
+                      </div>
                     </div>
-                  </div>
-                  <div class="messages__message--text">
-                    ${message.message}
-                  </div>
-                  <div class="messages__message--image">
-                    <img src="${message.image.url}" class="image-size">
+                    <div class="messages__message--text">
+                      ${message.message}
+                    </div>
+                    <div class="messages__message--image">
+                      <img src="${message.image.url}" class="image-size">
+                    </div>
                   </div>`
     }
     return html;
@@ -42,15 +52,16 @@ $(function() {
     })
     .done(function(data){
       var html = buildHTML(data);
+      $('.last_message').removeClass("last_message");
       $('.messages__message').append(html);
       $('.messages__post--textbox').val('');
-      $('.announce').empty();
-      $('.announce').append(`<div class="notice">メッセージを送信しました</div>`)
+      $("input[type='file']").val(null);
+      $('.messages__post--send').prop('disabled', false);
+      scrollLast();
     })
     .fail(function(){
-      $('.announce').empty();
-      $('.announce').append('<div class="alert">エラーが発生しました</div>')
-
+      alert("メッセージを入力してください")
+      $('.messages__post--send').prop('disabled', false);
     })
   });
 })

42f9f6ac7c4f33015958ee648c8a8e03c2231d0f
IiyamaMakoto <m.iiyama@gmail.com>
2019-10-24 11:15:13 +0900

    after-incremental-search-function
    実装項目：
    ・検索したユーザーをグループに追加する
    ・グループに追加したユーザーを削除する
    ・ログイン中のユーザーを常にグループに加える
    ・ログイン中のユーザーを削除できなくする
    ・グループ編集時、はじめからグループメンバーを表示させておく

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -17,6 +17,17 @@ $(function() {
     search_result.append(html);
   }
 
+  function addUser(user_name, user_id) {
+    var html = `
+                <div class='chat-group-user'>
+                  <input name='group[user_ids][]' type='hidden' value='${user_id}'>
+                  <p class='chat-group-user__name'>${user_name}</p>
+                  <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+                </div>
+                `
+    $('#chat-group-users').append(html);
+  }
+
 
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
@@ -41,5 +52,16 @@ $(function() {
     .fail(function(){
       alert('ユーザー検索に失敗しました');
     })
+  });
+
+  $(document).on('click', ".chat-group-user__btn--add", function(){
+    var user_id = $(this).data('user-id');
+    var user_name = $(this).data('user-name');
+    $(this).parent().remove();
+    addUser(user_name, user_id)
+  });
+
+  $(document).on('click', ".chat-group-user__btn--remove", function(){
+    $(this).parent().remove();
   })
 })
